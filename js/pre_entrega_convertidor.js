97b1291aca96be06b2e35ba04b2feba0bfb08f81
davidlongo <longodavidjulian@gmail.com>
2024-05-17 20:44:02 -0300

    probando ciclos anidados para volver a realizar la conversion de moneda

--- js/pre_entrega_convertidor.js
+++ js/pre_entrega_convertidor.js
@@ -20,30 +20,32 @@ function cambioDeMoneda() {
 
         if (tipoDeMoneda === "DOLAR") {
             resultado = dolarBlueAPesos(cantidad);
-            alert(cantidad + " son " + resultado + " pesos argentinos");
+            alert(cantidad + " son " + resultado + " pesos argentinos (ARS)");
         } else if (tipoDeMoneda === "PESOS") {
             resultado = pesosADolarBlue(cantidad);
-            alert(cantidad + " son " + resultado + " dólares americanos");
+            alert(cantidad + " son " + resultado + " dólares americanos (USD)");
         } else {
             alert("Ingrese una moneda correcta");
         }
     }
 };
 
-
 let nuevaOperacion = true;
+
 
 while (nuevaOperacion) {
     cambioDeMoneda();
     let respuestaUsuario = prompt("¿Quieres realizar otra operación? (responde 'si' para realizar otra operación, o 'no' para salir)");
+    
+    do {
+        respuestaUsuario = prompt("Por favor ingresa 'si' para realizar otra operación, o 'no' para salir.");
+    } while (respuestaUsuario.toLowerCase() !== "si" || respuestaUsuario.toLowerCase() !== "no");
 
     if (respuestaUsuario.toLowerCase() === "si") {
-        
-    } else if (respuestaUsuario.toLowerCase() === "no") {
+        nuevaOperacion = true;
+    } else {
         nuevaOperacion = false;
         alert("Gracias por utilizar nuestro servicio");
-    }else {
-        respuestaUsuario = prompt("Por favor ingresa 'si' para realizar otra operación, o 'no' para salir");
     }
 };
 
