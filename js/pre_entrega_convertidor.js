c7ae64baa7c99867bae8169b218c0c82322e3cd9
davidlongo <longodavidjulian@gmail.com>
2024-05-17 21:27:15 -0300

    Se corrigio el error en el que volvia a pedir que el usuario ingrese una respuesta, por mas que la respuesta sea correcta

--- js/pre_entrega_convertidor.js
+++ js/pre_entrega_convertidor.js
@@ -10,42 +10,45 @@ function dolarBlueAPesos(cantidad) {
 
 
 function cambioDeMoneda() {
-    let cantidad = parseFloat(prompt("Ingrese la cantidad de dinero a que desea cambiar"));
+    let cantidad = parseFloat(prompt("Ingrese la cantidad de dinero a que desea cambiar."));
     let resultado;
 
     if (isNaN(cantidad) || cantidad <= 0) {
         alert("Ingrese una cantidad válida");
     } else {
-        let tipoDeMoneda = prompt("Ingrese 'DOLAR' si quiere cambiar dólares a pesos, o, ingrese 'PESOS' si quiere cambiar pesos a dólares ");
+        let tipoDeMoneda = prompt("Ingrese 'DOLAR' si quiere cambiar dólares a pesos, o, ingrese 'PESOS' si quiere cambiar pesos a dólares.");
 
         if (tipoDeMoneda === "DOLAR") {
             resultado = dolarBlueAPesos(cantidad);
-            alert(cantidad + " son " + resultado + " pesos argentinos (ARS)");
+            alert(cantidad + " son " + resultado + " pesos argentinos (ARS).");
         } else if (tipoDeMoneda === "PESOS") {
             resultado = pesosADolarBlue(cantidad);
-            alert(cantidad + " son " + resultado + " dólares americanos (USD)");
+            alert(cantidad + " son " + resultado + " dólares americanos (USD).");
         } else {
-            alert("Ingrese una moneda correcta");
+            alert("Ingrese una moneda correcta.");
         }
     }
 };
 
-let nuevaOperacion = true;
 
+let nuevaOperacion = true;
 
 while (nuevaOperacion) {
     cambioDeMoneda();
-    let respuestaUsuario = prompt("¿Quieres realizar otra operación? (responde 'si' para realizar otra operación, o 'no' para salir)");
-    
-    do {
-        respuestaUsuario = prompt("Por favor ingresa 'si' para realizar otra operación, o 'no' para salir.");
-    } while (respuestaUsuario.toLowerCase() !== "si" || respuestaUsuario.toLowerCase() !== "no");
+    let respuestaUsuario = prompt("¿Quieres realizar otra operación? (responde 'si' para realizar otra operación, o 'no' para salir).");
 
+    do {
+        if (respuestaUsuario !== "si" && respuestaUsuario !== "no") {
+            respuestaUsuario = prompt("Por favor ingresa 'si' para realizar otra operación, o 'no' para salir.");
+        };
+    } while (respuestaUsuario.toLowerCase() !== "si" && respuestaUsuario.toLowerCase() !== "no");
+
+    
     if (respuestaUsuario.toLowerCase() === "si") {
-        nuevaOperacion = true;
+
     } else {
         nuevaOperacion = false;
-        alert("Gracias por utilizar nuestro servicio");
+        alert("Gracias por utilizar nuestro servicio.");
     }
 };
 
